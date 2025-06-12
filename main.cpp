803bf8ba2ff4250a2620d3d010e1833809c955f3
Tatiana M <tamendezquiros@gmail.com>
2025-06-11 20:36:45 -0600

    Primer commmit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std; 
+
+
+int main (){
+
+    cout<<"------------------------------------------------------------\n";
+    cout<< right << setw(40)<< "Bienvenido al programa"<<endl;
+    cout<<"------------------------------------------------------------\n";
+
+    return 0;
+}
